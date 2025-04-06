76e07e1e139d0f99fbcd03dc4e2517f2f3bfcb6b
ABHISHEK RAJ PATEL <technicalabhishekexpert@gmail.com>
2025-04-06 21:16:53 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 import joblib 
-
+ 
 
 # step 2: load dataset
 df = pd.read_csv('train.csv')
